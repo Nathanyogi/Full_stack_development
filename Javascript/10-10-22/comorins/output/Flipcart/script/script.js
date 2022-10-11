528df11037ab3de643c45accbba7a02aaaa36a1a
Nathanyogi <nathanm4122000@gmail.com>
2022-10-11 21:45:54 +0530

    Final Assessment day 2

--- Javascript/10-10-22/comorins/output/Flipcart/script/script.js
+++ Javascript/10-10-22/comorins/output/Flipcart/script/script.js
@@ -1,65 +1,230 @@
 let best_offer = {
     electronics :[{
         img:"https://rukminim1.flixcart.com/image/200/200/knyxqq80/dslr-camera/r/y/x/digital-camera-eos-m50-mark-ii-eos-m50-mark-ii-canon-original-imag2gzkexzqhyhu.jpeg?q=70",
-        device:"camera",
+        product:"Camera",
         price:"3999",
-        brands:"Canon,sony,"
-    },
-    {
-        img:"https://rukminim1.flixcart.com/image/200/200/printer/j/j/y/hp-laserjet-m1005-multifunction-original-imadxhzpeb9qbrfg.jpeg?q=70",
-        device:"printers",
-        price:"5000",
-        brands:"HP"
+        brands:"Canon,sony"
     },
     {
         img:"https://rukminim1.flixcart.com/image/200/200/ktszgy80/monitor/b/b/5/ed320qr-full-hd-um-je0ss-p01-acer-original-imag72hyazezew4b.jpeg?q=70",
-        device:"Monitors",
+        product:"Monitors",
         price:"7500",
         brands:"Lenovo"
     },
     {
-        img:"https://rukminim1.flixcart.com/image/200/200/l5jxt3k0/power-bank/b/p/m/power-bank-3-pd-22-5w-10000-rtx2108-realme-original-imagg7jckgt9zgag.jpeg?q=70",
-        device:"Power Bank",
+        img:"https://rukminim1.flixcart.com/image/200/200/kj7gwi80pkrrdj-0/trimmer-refurbished/o/r/u/a-device-of-man-ns-216-runtime-45-min-trimmer-for-men-women-original-imaftfg9hbedkwzu.jpeg?q=70",
+        product:"Trimmer",
         price:"2000",
         brands:"Mi,Realme"
     },
     {
         img:"https://rukminim1.flixcart.com/image/200/200/xif0q/projector/e/p/v/k9-pro-android-fhd-1080p-10-pre007-full-hd-egate-original-imagg9k9zauxwrfj.jpeg?q=70",
-        device:"Projector",
+        product:"Projector",
         price:"6990",
         brands:"Vega,Philips & More"
     },
     ],
     beauty_food_toys:[
         {
-            img:"https://rukminim1.flixcart.com/image/200/200/l4d2ljk0/bat/3/4/4/1000-hitman-full-size-double-blade-poplar-cricket-bat-advance-original-imagf9zzgxxfamce.jpeg?q=70",
-            product:"Cricket,Football & More",
-            Discount:"70%",
+            img:"https://www.pngall.com/wp-content/uploads/5/Cricket-Bat-PNG-Image.png",
+            product:"Sports",
+            price:"70%",
             brands:"Ceat,Nivia",
         },
         {
-            img:"https://rukminim1.flixcart.com/image/200/200/l2qhjm80/musical-keyboard/o/q/n/100-bigfun-37-key-piano-keyboard-toy-with-dc-power-niveeka-original-imageys6jwkgs4kw.jpeg?q=70",
+            img:"https://png.pngtree.com/png-vector/20191009/ourmid/pngtree-piano-keyboard-vector-png-image_1777158.jpg",
             product:"Musical Keyboard",
-            Discount:"70%",
+            price:"70%",
             brands:"Beston,Redbox & More",
         },
         {
             img:"https://rukminim1.flixcart.com/image/200/200/j3xbzww0/face-wash/4/4/s/150-purifying-neem-face-wash-himalaya-original-imaeuyfzgdmythzw.jpeg?q=70",
             product:"Skincare",
-            Discount:"40%",
+            price:"40%",
             brands:"Himalaya,MamaEarth & More",
         },
         {
             img:"https://rukminim1.flixcart.com/image/200/200/klzhq4w0/jam-spread/w/q/u/510-chocolate-peanut-butter-crunchy-510g-jar-nut-butter-original-imagyzpqqfaguxny.jpeg?q=70",
             product:"Food Spreads",
-            Discount:"75%",
+            price:"75%",
             brands:"Myfiness,Alpino & More",
+        }
+    ],
+}
+
+let product_list ={
+    camera:[
+        {
+            img:"https://rukminim1.flixcart.com/image/312/312/xif0q/instant-camera/r/k/t/instax-mini-11-moments-box-instax-mini-11-moments-box-instant-original-imaggmffkfwdmbh8.jpeg?q=70",
+            product:"FUJIFILM Instax Mini 11 Moments Box Instant Camera",
+            specification:["Grey","Exposure Mode: Automatic","View Finder: NO","Slef Timer: Yes","Battery Included","1 Year Warranty"],
+            offer_price:"6,999",
+            original_price:"8,999",
+            offer:"28%",
+            delivery:"Free Delivery"
+        },
+        {
+            img:"https://rukminim1.flixcart.com/image/312/312/kk01pjk0/dslr-camera/f/v/o/eos-1500d-canon-original-imafzfugydh2mjgf.jpeg?q=70",
+            product:"Canon EOS 1500D DSLR Camera Body+ 18-55 mm IS II Lens",
+            specification:["Black","Exposure Mode: Automatic","View Finder: Yes","Slef Timer: Yes","Battery Included","2 Year Warranty"],
+            offer_price:"28,990",
+            original_price:"33,999",
+            offer:"8%",
+            delivery:"Free delivery"
+        },
+        {
+            img:"https://rukminim1.flixcart.com/image/312/312/jm2c87k0/sports-action-camera/8/g/y/full-hd10-full-hd1080-p-ng-etc-full-hd10-happy2buy-original-imaf26jhfrg8qn8z.jpeg?q=70",
+            product:"RFV1 (tm) Full HD 1080P Sports DV Action Waterproof Sports and Action Camera",
+            specification:["Effective pixels 12MP","Full HD","no warranty"],
+            offer_price:"923",
+            original_price:"2,999",
+            offer:"69%",
+            delivery:"Free delivery"
+        },
+        {
+            img:"https://rukminim1.flixcart.com/image/312/312/l5fnhjk0/dslr-camera/f/t/m/eos-r10-24-2-r10-canon-original-imagg42fsbgv79da.jpeg?q=70",
+            product:"Canon EOS R10 Mirrorless Camera Body with RF-S 18 - 150 mm f/3.5 - 6.3 IS STM Lens Kit",
+            specification:["Effective pixels 24.2MP","Sensor Type: CMOS","WiFi Available","4K","2 Year Domestic warranty"],
+            offer_price:"1,13,566",
+            original_price:"1,17,999",
+            offer:"3%",
+            delivery:"Free Delivery"
+        },
+    ],
+    monitors:[
+        {
+            img:"https://rukminim1.flixcart.com/image/312/312/l5ld8y80/monitor/w/8/5/-original-imagg897bcqqhczw.jpeg?q=70",
+            product:"SAMSUNG 27 inch Curved Full HD VA Panel with 1800R Curvature, Game Mode Function, Eye-Saver Mode, Flic...",
+            specification:["Panel Type: VA Panel","Screen Resolution Type: Full HD","Brightness: 250ns","Response Time: 4ms | Refresh Rate: 60 Hz","3 Year warranty"],
+            offer_price:"12,699",
+            original_price:"24,700",
+            offer:"48%",
+            delivery:"Free delivary",
+
+        },
+        {
+            img:"https://rukminim1.flixcart.com/image/312/312/l3t2fm80/monitor/m/e/j/-original-imageudynqghyhxh.jpeg?q=70",
+            product:"MarQ by Flipkart 22 inch Full HD LED Backlit VA Panel with 2 X 3W Inbuilt Speakers Monitor (22FHDMVQII...",
+            specification:["Panel Type: VA Panel","Screen Resolution Type: Full HD","Brightness: 220nits","Response Time: 5ms | Refresh Rate: 75 Hz","1 Year warranty"],
+            offer_price:"6,899",
+            original_price:"12,999",
+            offer:"44%",
+            delivery:"Free delivery",
+        },
+        {
+            img:"https://rukminim1.flixcart.com/image/312/312/xif0q/monitor/o/8/v/ka222q-b-full-hd-21-5-um-wx2si-b01-acer-original-imagg4fgmeb8jf4j.jpeg?q=70",
+            product:"acer 21.5 inch Full HD VA Panel Monitor (KA222Q B)",
+            specification:["Panel Type: VA Panel","Screen Resolution Type: Full HD","Brightness: 250nits","Response Time: 5ms | Refresh Rate: 75 Hz","3 Years warranty"],
+            offer_price:"6,899",
+            original_price:"10,900",
+            offer:"36%",
+            delivery:"Free delivary",
+        },
+        {
+            img:"https://rukminim1.flixcart.com/image/312/312/l5ld8y80/monitor/l/k/s/-original-imagg897ufhyvwqq.jpeg?q=70",
+            product:"SAMSUNG 24 inch Full HD LED Backlit IPS Panel with 3-Sided Borderless Display, Game &amp; Free Sync Mode, ...",
+            specification:["Panel Type: IPS Panel","Screen Resolution Type: Full HD","Brightness: 250nits","Response Time: 5ms | Refresh Rate: 75 Hz","3 Years warranty"],
+            offer_price:"9,899",
+            original_price:"19,100",
+            offer:"58%",
+            delivery:"Free delivery",
+        },
+    ],
+    projector:[
+        {
+            img:"https://rukminim1.flixcart.com/image/612/612/l1dwknk0/projector/7/a/q/latest-2022-mp11-full-hd-native-android-bluetooth-home-theater-original-imagcyswwfazpxfh.jpeg?q=70",
+            product:"PLAY Latest 2022 MP11 Full HD Native Android Bluetooth Home theater cinema Projector (6500 lm / Wireless / Remote Controller) Portable Projector",
+            specification:["Silvar","Portable: Yes","Internal Storage: 8192 MB","Maximum Brightness: 6500lm"],
+            offer_price:"24,819",
+            original_price:"1,50,000",
+            offer:"83%",
+            delivery:"Free delivery",
+
+        },
+        {
+            img:"https://rukminim1.flixcart.com/image/612/612/kx50gi80/projector/r/9/p/zeb-pixa-play-12-5-6-dobly-audio-led-projector-zebronics-original-imag9z3yujqmzqt4.jpeg?q=70",
+            product:"ZEBRONICS Zeb-Pixa Play 12 with Dolby Audio Support &amp; 720p HD (3000 lm / 1 Speaker / Remote Controller) Projector&nbsp;&nbsp;(Black)",
+            specification:["Black","Portable: No","Internal Storage: 8192 MB","Maximum Brightness: 3000lm"],
+            offer_price:"8,899",
+            original_price:"21,999",
+            offer:"59%",
+            delivery:"Free delivery",
+        },
+        {
+            img:"https://rukminim1.flixcart.com/image/612/612/kyj0vbk0/projector/r/n/r/-original-imagaqky6yftfmn8.jpeg?q=70",
+            product:"ZEBRONICS ZEB-PIXAPLAY 15 with Android 9.0, Full HD 1080p, WiFi/BT v5.1 (3400 lm / 1 Speaker / Remote Controller) Projector &nbsp;&nbsp;(Black)",
+            specification:["Black","Portable: No","Internal Storage: 8192 MB","Maximum Brightness: 3000lm"],
+            offer_price:"15,399",
+            original_price:"42,999",
+            offer:"64%",
+            delivery:"Free delivery",
+        },
+    ],
+    trimmers:[
+        {
+            img:"https://rukminim1.flixcart.com/image/612/612/kdnf98w0-0/trimmer/s/y/e/0-5-10-mm-stainless-steel-xxq02hm-cordless-mi-original-imafug358t8cxzzk.jpeg?q=70",
+            product:"Mi XXQ02HM Trimmer 60 min  Runtime 20 Length Settings &nbsp;&nbsp;(Black)",
+            specification:["Blade Material: Stainless Steel","Timming Range: 0.5-10 mm","60 min battery run time","Gender: men"],
+            offer_price:"799",
+            original_price:"1,999",
+            offer:"33%",
+            delivery:"Free delivery",
+        },
+        {
+            img:"https://rukminim1.flixcart.com/image/612/612/kyrlifk0/trimmer/b/n/4/-original-imagaxgncgcxndjj.jpeg?q=70",
+            product:"PHILIPS BT3231/15 Trimmer 60 min  Runtime 20 Length Settings &nbsp;&nbsp;(Green)",
+            specification:["Blade Material: Stainless Steel","Timming Range: 0.5-10 mm","60 min battery run time","Gender: men"],
+            offer_price:"1,125",
+            original_price:"1,995",
+            offer:"37%",
+            delivery:"Free delivery",
+        },
+        {
+            img:"https://rukminim1.flixcart.com/image/612/612/kuof5ow0/trimmer/m/q/v/0-5-7-mm-bt1232-15-stainless-steel-cordless-philips-original-imag7r4rh8ewvfnr.jpeg?q=70",
+            product:"PHILIPS BT1232/15 Trimmer 30 mins  Runtime 3 Length Settings &nbsp;&nbsp;(Blue)",
+            specification:["Blade Material: Stainless Steel","Timming Range: 0.5-7 mm","30 min battery run time","Gender: men"],
+            offer_price:"644",
+            original_price:"995",
+            offer:"35%",
+            delivery:"Free delivery",
         },
     ]
 }
-function createkey(key){
-    if(! localStorage.getItem(key)){
-        localStorage.setItem(key,JSON.stringify([]))
+function createkey(localkey,localvalue){
+    if(! localStorage.getItem(localkey)){
+        localStorage.setItem(localkey,JSON.stringify([]))
+        
+    }
+    let dummy_array = JSON.parse(localStorage.getItem(localkey));
+    dummy_array.push(localvalue);
+    localStorage.setItem(localkey,JSON.stringify(dummy_array))
+}
+createkey("top_offer",best_offer);
+
+
+function showItem(localkey,key){
+    let list = JSON.parse(localStorage.getItem(localkey));
+    let ele = "";
+    for(let i = 0 ; i<list[i][key].length;i++){
+        ele += "<div class='card col text-center pt-2 el-col'>"+"<img class ='card-img-top' src="+list[i][key][i].img+ ">" + "<div class='card-body'><h4 class='card-text'>"+list[i][key][i].product+"</h4>"+ "<p class='text-success'>Offer price: "+list[i][key][i].price + "</p>"+  "<p class='text-secondary'>"+list[i][key][i].brands+ "</p>" +"<a class='btn btn-sm btn-primary' target='_blank' href=products.html?name="+list[i][key][i].product+">View Products</a>"+ "</div>"+ "</div>"
+    }
+    if(key == "electronics"){
+        document.getElementById('elec-product-offers').innerHTML = ele
+        console.log(list[0][key].length)
+    }
+    else if(key == "beauty_food_toys"){
+        document.getElementById('food-beauty-offers').innerHTML = ele
+        
+        // document.getElementById('price-content').textContent = "Discount"
+        console.log(list[1][key].length)
+    }
+    else{
+        alert('product is not update in object')
     }
 }
-createKey("top_offer")
+showItem("top_offer","electronics")
+showItem('top_offer',"beauty_food_toys")
+// document.getElementById("img").src= list[i].electronics[j].img
+// document.getElementById("device").textContent = list[i].electronics[j].device
+// document.getElementById("price").textContent = list[i].electronics[j].price
+// document.getElementById("brands").textContent = list[i].electronics[j].brands
